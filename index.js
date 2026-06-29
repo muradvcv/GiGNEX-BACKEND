89b6bd782456014538a26d5e53ee29d208168faa
muradvcv <muradvcv@gmail.com>
2026-06-29 13:00:18 +0600

    error

--- index.js
+++ index.js
@@ -728,3 +728,4 @@ run().catch(console.dir);
 // app.listen(process.env.PORT || 5000, () => {
 //   console.log(`Server is running on port ${process.env.PORT || 5000}`);
 // });
+module.exports = app;
